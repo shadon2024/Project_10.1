221fc61f4651b600fc1d472f2c57e905b8149407
shadon2024 <in-con23@yandex.ru>
2024-06-06 12:04:54 +0500

    Initial Commit

---
+++ M15/MyViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MyViewController.swift
+//  M15
+//
+//  Created by Admin on 25/04/24.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+    // MARK: - Navigation
+
+}
---
+++ M15/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  M15
+//
+//  Created by Admin on 17/04/24.
+//
+
+import UIKit
+import SideMenu
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
